c8fb6d96a7a90c9e9e45344c09e6805c6024cb55
RimJoudi <77971241+RimJoudi@users.noreply.github.com>
2021-06-30 20:12:40 +0100

    pep8 fixed

--- models/__init__.py
+++ models/__init__.py
@@ -8,24 +8,3 @@ from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-Update models/__init__.py: to create a unique FileStorage instance for your application
-
-    import file_storage.py
-    create the variable storage, an instance of FileStorage
-    call reload() method on this variable
-"""
-
